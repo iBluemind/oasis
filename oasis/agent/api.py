ac27256b321dc825822c07b372750fa75f592620
minkikim89 <minkikim89@gmail.com>
2016-09-25 16:47:45 +0900

    add agent config

--- oasis/agent/api.py
+++ oasis/agent/api.py
@@ -5,15 +5,18 @@ from oslo_config import cfg
 class API(rpc_service.API):
     def __init__(self, transport=None, context=None, topic=None):
         if topic is None:
-            cfg.CONF.import_opt('topic', 'oasis.conductor.config',
-                                group='conductor')
+            cfg.CONF.import_opt('topic', 'oasis.agent.config',
+                                group='agent')
         super(API, self).__init__(transport, context,
                                   topic=cfg.CONF.conductor.topic)
 
     # Function Operations
-
     def function_create(self, function):
         return self._call('function_create', function=function)
+
+    # Endpoint Operations
+    def endpoint_create(self, function, node_pool):
+        pass
 
 
 
